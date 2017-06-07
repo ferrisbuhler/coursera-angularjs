475d2dc5bc35a1cc497d78f808431a095f57c35e
Matthias Buhler <gitlab.com@matthias-buhler.de>
2017-06-07 22:55:43 +0200

    preped assignment 3

---
+++ assignment3/app.js
@@ -0,0 +1,61 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+
+MenuCategoriesController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menu = this;
+
+  var promise = MenuSearchService.getMenuCategories();
+
+  promise.then(function (response) {
+    menu.categories = response.data;
+  })
+  .catch(function (error) {
+    console.log("Something went terribly wrong.");
+  });
+
+  menu.logMenuItems = function (shortName) {
+    var promise = MenuSearchService.getMenuForCategory(shortName);
+
+    promise.then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
+
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+      params: {
+        category: shortName
+      }
+    }).then(function (result) {
+        // process result and only keep items that match
+        var foundItems...
+
+        // return processed items
+        return foundItems;
+    });
+
+    return response;
+  };
+
+}
+
+})();
